d87beca87841f270096dd71b0d8c9893515f058a
Karl <karl111@protonmail.com>
2023-10-09 17:41:54 +0300

    Update Discord_GWorkspace_Auth.py

--- Discord_GWorkspace_Auth.py
+++ Discord_GWorkspace_Auth.py
@@ -30,7 +30,7 @@ GROUP_ROLE_PAIRS = {
     "vilistlased@robotiklubi.ee": "Vilistlane",
     # "kursandid@robotiklubi.ee": "Kursant"
 }
-global_GROUP_MEMBER_PAIRS = {}
+global_group_member_pairs = {}
 
 
 class Bot:
@@ -105,7 +105,7 @@ class Bot:
                 'https://www.googleapis.com/userinfo/v2/me').json()
 
             user_in_groups = []
-            for email, members in global_GROUP_MEMBER_PAIRS.items():
+            for email, members in global_group_member_pairs.items():
                 if profile_info['email'] in members:
                     user_in_groups.append(email)
 
@@ -139,7 +139,7 @@ class Bot:
                 for member in members:
                     member_emails.append(member.get('email', []))
                 # Update groups list
-                global_GROUP_MEMBER_PAIRS[email] = member_emails
+                global_group_member_pairs[email] = member_emails
 
         # INITIALIZE DISCORD CLIENT
         self.client = discord.Client(intents=intents)
@@ -197,7 +197,7 @@ class Bot:
                     await refresh_members_list(message)
 
                     await message.author.send(
-                        f"Keys: {global_GROUP_MEMBER_PAIRS.keys()}\nSuccessfully loaded members lists")
+                        f"Keys: {global_group_member_pairs.keys()}\nSuccessfully loaded members lists")
 
         # Run the bot
         self.client.run(token)
